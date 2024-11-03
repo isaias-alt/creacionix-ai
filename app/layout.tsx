7cd13a68ac56b261dfce547f83e76a608308fbc0
isaias-alt <cascolucasisaias@gmail.com>
2024-11-03 11:56:56 -0300

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,51 @@ const outfit = Outfit({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Creacionix AI",
   description: "Revolutionize your content creation with our AI-powered app.",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://creacionix-ai.vercel.app/",
+    siteName: "Creacionix AI",
+    title: "Creacionix AI",
+    description: "Revolutionize your content creation with our AI-powered app.",
+    images: [
+      {
+        url: 'https://creacionix-ai.vercel.app/og.png',
+        width: 1200,
+        height: 630,
+        alt: "Creacionix AI",
+      },
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  twitter: {
+    title: "Creacionix AI",
+    description: "Revolutionize your content creation with our AI-powered app.",
+    images: [
+      {
+        url: 'https://creacionix-ai.vercel.app/og.png',
+        width: 1200,
+        height: 630,
+        alt: "Creacionix AI",
+      },
+    ],
+    card: "summary_large_image",
+    site: "@creacionix_ai",
+    creator: "@lucascodev",
+  },
+  verification: {
+    google: "https://creacionix-ai.vercel.app/",
+  },
 };
 
 export default function RootLayout({

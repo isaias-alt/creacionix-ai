3f55a5ac5b0a0dff1716ee9eadb4155bf608bb26
isaias-alt <cascolucasisaias@gmail.com>
2024-11-02 22:35:37 -0300

    disable eslint no-empty-object-type

--- components/ui/textarea.tsx
+++ components/ui/textarea.tsx
@@ -1,10 +1,8 @@
-/* eslint-disable @typescript-eslint/no-empty-object-type */
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
-export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> { }
+export interface TextareaProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement> { }
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
   ({ className, ...props }, ref) => {
